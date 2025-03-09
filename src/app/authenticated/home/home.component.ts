d97c19c935109747b46be453c4ad97283f4af1ef
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-03-09 23:54:40 +0800

    add footer and add design for clicking copy clipboard

--- src/app/authenticated/home/home.component.ts
+++ src/app/authenticated/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  public isClicked = false;
+
+  private timeOutId : any;
+
+  public changeStyle() : void{
+
+    if(this.timeOutId)
+      clearTimeout(this.timeOutId);
+    
+    this.isClicked = true;
+
+    this.timeOutId = setTimeout(()=>{
+      this.isClicked = false;
+    },2000);
+
+  }
 
 }
