f112edf2d29ae8c4c39d874ef0f6070177a3b551
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-03-11 14:11:08 +0800

    add simple validator for username and password in signin and inject authService

--- src/app/components/authentication/signin/signin.component.ts
+++ src/app/components/authentication/signin/signin.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthenticationService } from '../../../security/services/authentication.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginDTO } from '../../../models/login.dto';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-signin',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './signin.component.html'
 })
 export class SigninComponent {
+  private authService = inject(AuthenticationService);
+
+  signinForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+  });
+  
+  public login() : void{
+
+    if(!this.signinForm.valid){
+      console.warn('Not valid');
+      return;
+    }
+
+    const loginDTO = this.signinForm.getRawValue() as LoginDTO;
+
+    this.authService
+      .logIn(loginDTO)
+      .pipe(first())
+      .subscribe({
+        next : (response : any) =>{
+
+          console.log(response);
+
+        },
+        error : (error : any) =>{
+          console.error(error);
+        }
+      });
+
+  }
 
 }
--- src/app/security/services/authentication.service.ts
+++ src/app/security/services/authentication.service.ts
@@ -1,16 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginDTO } from '../../models/login.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  private isLoggedIn : boolean = false;
+  private isLoggedIn = signal(false);
 
-  constructor() { }
+  private localHost : string = 'http://localhost:8080';
+
+  constructor(
+    private httpClient : HttpClient
+  ) { }
 
   public getIsLoggedIn() : boolean{
-    return this.isLoggedIn;
+    return this.isLoggedIn();
+  }
+
+  public logIn(body : LoginDTO) : Observable<any>{
+    return this.httpClient.post<any>(`${this.localHost}/api/authentication/login`,body);
   }
 
 }
