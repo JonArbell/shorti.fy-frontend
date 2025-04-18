99757da640db0b3569e2d77d6f6415666c96a243
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-04-18 20:25:34 +0800

    add sweet alert 2 and logout modal

--- src/app/views/authenticated/side-nav/side-nav.component.ts
+++ src/app/views/authenticated/side-nav/side-nav.component.ts
@@ -1,9 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AuthService } from '../../../services/authentication/auth.service';
 @Component({
   selector: 'app-side-nav',
   imports: [RouterLinkActive, RouterLink],
   templateUrl: './side-nav.component.html',
 })
-export class SideNavComponent {}
+export class SideNavComponent {
+
+  constructor(
+    private router : Router,
+    private authService : AuthService
+  ){}
+
+
+  logout() : void{
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will be logged out!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6', // Blue
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, log me out!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.clear();
+
+        this.authService.isAuthenticated.set(false);
+        this.router.navigate(['/']);
+        Swal.fire(
+          'Logged Out!',
+          'You have been successfully logged out.',
+          'success'
+        );
+      }
+    });
+  }
+
+}
