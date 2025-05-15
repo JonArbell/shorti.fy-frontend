bfa44fbec7e5adfa47a15134cfda98bb36ec5e52
JonArbell <deocampo.arbelldonor@gmail.com>
2025-05-15 17:07:19 +0800

    panis

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   // AUTHENTICATION_BASE_URL: 'http://localhost:8080/api/authentication',
   // AUTHENTICATED_BASE_URL: 'http://localhost:8080/api/authenticated',
 
-  AUTHENTICATION_BASE_URL: 'http://192.168.110.242:8080/api/authentication',
-  AUTHENTICATED_BASE_URL: 'http://192.168.110.242:8080/api/authenticated',
-  REDIRECT_BASE_URL: 'http://192.168.110.242:8080',
+  AUTHENTICATION_BASE_URL: 'http://192.168.0.114:8080/api/authentication',
+  AUTHENTICATED_BASE_URL: 'http://192.168.0.114:8080/api/authenticated',
+  REDIRECT_BASE_URL: 'http://192.168.0.114:8080',
 };
